1a7b1a21288774918f7c13cde161654fb2253ef0
qianmuuq <51996699+qianmuuq@users.noreply.github.com>
2021-01-11 18:11:37 +0800

    Update robotArm.js

--- static/js/robot/robotArm.js
+++ static/js/robot/robotArm.js
@@ -295,6 +295,12 @@ function basec(){
 //----------------------------------------------------------------------------
 function wave(){
     flag=0;
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+    based();
+    headd();
+    armright();
+    armleft();
+    leg();
     waver(0);
 }
 
